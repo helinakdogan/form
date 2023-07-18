94d5d15cc3d3ade9cffc7f247647eb0b87d166eb
Helin Akdoğan <akdoganhelin@gmail.com>
2023-07-18 17:27:00 +0300

    18.07.2023

--- src/App.js
+++ src/App.js
@@ -1,157 +1,73 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import Select from "react-select";
-import './App.css';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import "./App.css";
 
-const RegisterForm = () => {
-  const options = [
-    { value: 'woman', label: 'woman' },
-    { value: 'man', label: 'man' },
-];
-  //const [selected, setSelected] = useState(""); //options[0]
+function App() {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+  const [gender, setGender] = React.useState("");
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const handleRegistration = (data) => console.log(data);
-  const handleError = (errors) => {};
-
-  const registerOptions = {
-    firstname: { required: "First name is required" },
-    surname: { required: "Surname is required" },
-    gender: { required: "Gender is required" },
-    email: { required: "Email is required" },
-    password: {
-      required: "Password is required",
-      minLength: {
-        value: 8,
-        message: "Password must have at least 8 characters",
-      },
-    },
+  const genderHandler = (event) => {
+    setGender(event.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
   };
 
-  const submitHandler = () => {
-    console.log("surname");
-  }
-
   return (
-    <form onSubmit={handleSubmit(handleRegistration, handleError)}>
-      <div>
-        <label>First Name</label>
-        <input
-          firstname="firstname"
-          type="text"
-          {...register("firstname", registerOptions.firstname)}
-        />
-        <small className="text-danger">
-          {errors?.firstname && errors.firstname.message}
-        </small>
-      </div>
+    <form onSubmit={handleSubmit}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          
+          <Grid item xs={8}>
+            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+              <InputLabel id="demo-select-small-label">Age</InputLabel>
+              <Select
+                labelId="demo-select-small-label"
+                id="demo-select-small"
+                value={gender}
+                label="Age"
+                onChange={genderHandler}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
 
-      <div>
-        <label>Surname</label>
-        <input
-          surname="firstname"
-          type="text"
-          {...register("surname", registerOptions.surname)}
-        />
-        <small className="text-danger">
-          {errors?.surname && errors.surname.message}
-        </small>
-      </div>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
 
-      <div>
-        <label>Gender</label>
-        {/*<select value={selected} onChange={(e) => setSelected(e.target.value)}>
-          {options.map((value) => (
-            <option value={value} key={value}>
-              {value}
-            </option>
-          ))}
-          </select>*/}
-        <Select
-          value={options.value}
-          options={options}
-          defaultValue={""}
-        />
-          <small className="text-danger">
-            {options.value==="" && errors?.gender && errors.gender.message}      
-        </small>
-        
-        
-      </div>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
 
-      <div>
-        <label>Email</label>
-        <input
-          type="email"
-          name="email"
-          placeholder="optional"
-          {...register("email", registerOptions.email)}
-        />
-      </div>
-
-      <div>
-        <label>Project Name</label>
-        <input
-          surname="projectName"
-          type="text"
-          placeholder="optional"
-          {...register("projectName", registerOptions.surname)}
-        />
-      </div>
-
-      <div>
-        <label>Office Name</label>
-        <input
-          surname="officeName"
-          type="text"
-          placeholder="optional"
-          {...register("officeName", registerOptions.officeName)}
-        />
-      </div>
-
-      <div>
-        <label>Title</label>
-        <input
-          surname="title"
-          type="text"
-          placeholder="optional"
-          {...register("title", registerOptions.title)}
-        />
-      </div>
-
-      <div>
-        <label>Password</label>
-        <input
-          type="password"
-          name="password"
-          {...register("password", registerOptions.password)}
-        />
-        <small className="text-danger">
-          {errors?.password && errors.password.message}
-        </small>
-      </div>
-
-      <div>
-        <label>Password Again</label>
-        <input
-          type="password"
-          name="password"
-          {...register("password", registerOptions.password)}
-        />
-        <small className="text-danger">
-          {errors?.password && errors.password.message}
-        </small>
-      </div>
-
-      <button onChange={submitHandler}>Submit</button>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
+      </Box>
     </form>
   );
-};
-export default RegisterForm;
+}
+export default App;
 
 /*
 react state nedir, state ve set state kullanımına örnek
